02875e42e075fd0b1a5fc2a258779ce53a5cd0c2
Lou <richard_glz12@icloud.com>
2026-05-17 12:55:21 -0600

    usuario server10

--- app/Models/DocumentoUsuario.php
+++ app/Models/DocumentoUsuario.php
@@ -4,13 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Documentousuario extends Model
+class DocumentoUsuario extends Model
 {
     protected $table = 'Documentos_usuario';
     protected $fillable = ['Id_usuario', 'nombre_documento', 'ruta_archivo', 'validado'];
 
     public function usuario()
     {
-        return $this->belongsTo(usuario::class, 'Id_usuario', 'Id_usuario');
+        return $this->belongsTo(Usuario::class, 'Id_usuario', 'Id_usuario');
     }
 }
