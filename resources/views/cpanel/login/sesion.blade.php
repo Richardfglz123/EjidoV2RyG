16bc4d468981a7ec2fcec0d00af6e8af20d4cc14
Lou <richard_glz12@icloud.com>
2026-05-18 17:57:17 -0600

    Final?

--- resources/views/cpanel/login/sesion.blade.php
+++ resources/views/cpanel/login/sesion.blade.php
@@ -241,7 +241,7 @@
 
             <div class="col-md-4 text-center text-md-start">
                 <img src="{{ asset('SnRafael.png') }}" alt="Logo" height="50" class="mb-2">
-                <h6 class="text-uppercase fw-bold mb-0">Sistema Ejidal San </h6>
+                <h6 class="text-uppercase fw-bold mb-0">Sistema Ejidal San Rafael Ixtapalucan </h6>
             </div>
 
             <div class="col-md-4 text-center my-3 my-md-0">
