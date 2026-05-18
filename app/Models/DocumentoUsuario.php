610532293de12e2df4d5e849824a0cfac6f64498
Lou <richard_glz12@icloud.com>
2026-05-18 16:24:46 -0600

    expediente3

--- app/Models/DocumentoUsuario.php
+++ app/Models/DocumentoUsuario.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Documentousuario extends Model
+class DocumentoUsuario extends Model
 {
     protected $table = 'Documentos_Usuario';
 
