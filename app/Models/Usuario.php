4c8ff9a0427cc8064c0b7696f6a51d3546754746
Lou <richard_glz12@icloud.com>
2026-05-18 11:16:57 -0600

    documentos fix jeje

--- app/Models/Usuario.php
+++ app/Models/Usuario.php
@@ -10,17 +10,11 @@ class Usuario extends Authenticatable
 {
     use HasApiTokens, Notifiable;
 
-    // 1. Indica el nombre real de tu tabla
     protected $table = 'usuario';
 
-    // 2. INDICA TU LLAVE PRIMARIA (Esto arregla el error del tokenable_id)
     protected $primaryKey = 'Id_Usuario';
-
-    // 3. Si tu Id_Usuario es un INT autoincrementable, asegúrate de esto:
     public $incrementing = true;
 
-    // 4. Desactiva timestamps si no tienes las columnas 'created_at' y 'updated_at'
-    // o mapealas si tienen nombres distintos (como Fecha_Creo)
     const CREATED_AT = 'Fecha_Creo';
     const UPDATED_AT = 'Fecha_Modificado';
 
@@ -32,4 +26,8 @@ class Usuario extends Authenticatable
     protected $hidden = [
         'Contraseña',
     ];
+    public function documentos()
+    {
+        return $this->hasMany(DocumentoUsuario::class, 'Id_Usuario', 'Id_Usuario');
+    }
 }
