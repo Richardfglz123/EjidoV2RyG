60a925175515f6575520bc119082c8c3ca41ecfa
Lou <richard_glz12@icloud.com>
2026-05-18 11:06:16 -0600

    Oso

--- resources/views/cpanel/plantilla.blade.php
+++ resources/views/cpanel/plantilla.blade.php
@@ -51,7 +51,7 @@
             </button>
 
             <a class="navbar-brand d-flex align-items-center gap-2 m-0 p-0" href="{{ route('inicio') }}">
-                <img src="/snRafael.png" alt="Logo" height="35">
+                <img src="/app/public/snRafael.png" alt="Logo" height="35">
                 <span class="fw-bold d-none d-sm-inline" style="font-size: 1rem;">Sistema Ejidal San Rafael Ixtapalucan</span>
             </a>
         </div>
