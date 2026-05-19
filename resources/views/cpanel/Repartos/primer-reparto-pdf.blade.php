a8c45058c030e39e8e2261338d02ddaab399d722
Lou <richard_glz12@icloud.com>
2026-05-19 07:49:47 -0600

    final?3

--- resources/views/cpanel/Repartos/primer-reparto-pdf.blade.php
+++ resources/views/cpanel/Repartos/primer-reparto-pdf.blade.php
@@ -90,14 +90,14 @@
 
     <div class="info-section">
         <div class="info-row">
-            <p><strong></strong><br>
-                Folio: #{{ str_pad($prestamo->Id_Prestamo, 5, '0', STR_PAD_LEFT) }}</p>
-            </strong><br>
-            Año de Gestión: {{ now()->year }}</p>
+            <span class="label">Fecha Inicial:</span>
+            <span>{{ \Carbon\Carbon::parse($prestamo->Fecha)->format('d/m/Y H:i') }}</span>
         </div>
-        <span class="fw-bold" style="text-transform: uppercase; font-size: 11px;">
-            {{ $ejidatario->Nombres }} {{ $ejidatario->Apellido_Paterno }} {{ $ejidatario->Apellido_Materno }}
-        </span>
+
+        <span class="fw-bold">
+    {{ $prestamo->ejidatario?->usuario?->Nombres ?? 'Ejidatario' }}
+            {{ $prestamo->ejidatario?->usuario?->Apellido_Paterno ?? '' }}
+</span>
     </div>
 
     <!-- SECCIÓN DE DETALLE ORIGINAL -->
