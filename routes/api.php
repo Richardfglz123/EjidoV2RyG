7b628804bb6e295ea843761e55da0645cc59d5b6
Lou <richard_glz12@icloud.com>
2026-05-20 17:40:33 -0600

    correccion

--- routes/api.php
+++ routes/api.php
@@ -7,23 +7,26 @@ use App\Http\Controllers\EjidatariosController;
 use App\Http\Controllers\UsuariosController;
 use App\Http\Controllers\EventoController;
 use App\Http\Controllers\PaseListaController;
-
 use App\Models\Evento;
 
 // RUTAS PÚBLICAS
 Route::get('/ping', function () { return response()->json(['ok' => true]); });
 Route::post('/login', [ApiController::class, 'login']);
 Route::post('/verifyCode', [ApiController::class, 'verifyCode']);
+
+//  RUTAS DE USUARIOS PÚBLICAS:
+Route::get('/usuarios', [UsuariosController::class, 'apiIndex']);
+Route::post('/usuarios', [UsuariosController::class, 'apiStore']);
+Route::put('/usuarios/{id}', [UsuariosController::class, 'apiUpdate']);
+Route::delete('/usuarios/{id}', [UsuariosController::class, 'apiDestroy']);
+
 Route::get('/ejidatarios', [EjidatariosController::class, 'getEjidatariosApi']);
 Route::post('/eventos/nuevo', [EventoController::class, 'storeApi']);
 Route::get('/eventos', function () { return response()->json(\App\Models\Evento::all()); });
 Route::match(['get', 'post'], '/asistencia/registrar', [PaseListaController::class, 'marcarAsistencia']);
 
+
 // RUTAS PROTEGIDAS
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('/user-profile', [PerfilController::class, 'getPerfilApi']);
-    Route::get('/usuarios', [UsuariosController::class, 'apiIndex']);
-    Route::post('/usuarios', [UsuariosController::class, 'apiStore']);
-    Route::put('/usuarios/{id}', [UsuariosController::class, 'apiUpdate']);
-    Route::delete('/usuarios/{id}', [UsuariosController::class, 'apiDestroy']);
 });
