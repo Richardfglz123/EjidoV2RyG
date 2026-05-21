3b366a41f9550c46a3ad1a44f83f626a4e35ea1b
Lou <richard_glz12@icloud.com>
2026-05-21 10:40:32 -0600

    utilidades

--- routes/web.php
+++ routes/web.php
@@ -318,18 +318,26 @@ Route::prefix('configuracion')->group(function () {
 });
 
 
-//  MÓDULO GESTION DE EXPEDIENTES
-Route::prefix('admon/expedientes')->group(function () {
+// MÓDULO: VER ARCHIVOS (Helper)
+Route::get('ver-expediente/{path}', function ($path) {
+    $path = urldecode($path);
+    $cleanPath = str_replace('storage/', '', $path);
+    if (!Storage::disk('public')->exists($cleanPath)) {
+        abort(404, 'El archivo físico no existe.');
+    }
+    return Storage::disk('public')->response($cleanPath);
+})->where('path', '.*')->name('ver.expediente');
 
+// MÓDULO GESTIÓN DE EXPEDIENTES
+Route::prefix('admon/expedientes')->group(function () {
     Route::get('/', [ExpedienteController::class, 'index'])->name('expedientes.index');
 
     Route::middleware(['permiso:expedientes_crear'])->group(function () {
-        // Renombramos las rutas para que coincidan con tu vista
-        Route::get('/nuevo', [ExpedienteController::class, 'create'])->name('ejidatarios.create');
-        Route::post('/', [ExpedienteController::class, 'store'])->name('ejidatarios.store');
-        Route::get('/{id}/editar', [ExpedienteController::class, 'edit'])->name('ejidatarios.edit');
-        Route::put('/{id}', [ExpedienteController::class, 'update'])->name('ejidatarios.update');
-        Route::delete('/{id}', [ExpedienteController::class, 'destroy'])->name('ejidatarios.destroy');
+        Route::get('/nuevo', [ExpedienteController::class, 'create'])->name('expedientes.create');
+        Route::post('/', [ExpedienteController::class, 'store'])->name('expedientes.store');
+        Route::get('/{id}/editar', [ExpedienteController::class, 'edit'])->name('expedientes.edit');
+        Route::put('/{id}', [ExpedienteController::class, 'update'])->name('expedientes.update');
+        Route::delete('/{id}', [ExpedienteController::class, 'destroy'])->name('expedientes.destroy');
     });
 });
 
