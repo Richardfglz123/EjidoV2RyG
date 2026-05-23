aca569663fc34629e99d5eefc36ece3397671d37
Lou <richard_glz12@icloud.com>
2026-05-22 23:31:28 -0600

    cambios

--- resources/views/cpanel/plantilla.blade.php
+++ resources/views/cpanel/plantilla.blade.php
@@ -332,7 +332,7 @@
             <div class="col-md-4 text-center text-md-start">
                 <img src="{{ asset('SnRafael.png') }}" alt="Logo" height="50" class="mb-2">
                 <h6 class="text-uppercase fw-bold mb-0">Sistema de Gestión Ejidal San Rafael Ixtapalucan</h6>
-                <small class="text-secondary">v1.4.1</small>
+                <small class="text-secondary">v1.2.1</small>
             </div>
 
             <div class="col-md-4 text-center my-3 my-md-0">
